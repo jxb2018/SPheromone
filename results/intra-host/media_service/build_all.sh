fe064aefff8898632a931606c816db656b8f7949
jxb2018 <1369375419@qq.com>
2023-04-21 08:57:59 +0800

    + scripts inter-host media_service

---
+++ results/intra-host/media_service/build_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ssh -p 22 lgw@192.168.1.129 "cmake --build /tmp/SPheromone129 \
+--target manager coordinator scheduler executor \
+exp02_store_review_4 exp02_upload_text_3 exp02_upload_uniqueid_3 exp02_upload_rating_3 exp02_upload_movieid_3 \
+exp02_upload_rating_2 exp02_upload_uniqueid_1 exp02_upload_text_1 exp02_upload_movieid_1 exp02_register_movieid_1 \
+exp02_web_server_0 exp02_register_test1 exp02_invoker_test1 -- -j 16"
+
+#ssh -p 22 lgw@192.168.1.126 "cmake --build /tmp/SPheromone126 \
+#--target manager coordinator scheduler executor \
+#exp02_store_review_4 exp02_upload_text_3 exp02_upload_uniqueid_3 exp02_upload_rating_3 exp02_upload_movieid_3 \
+#exp02_upload_rating_2 exp02_upload_uniqueid_1 exp02_upload_text_1 exp02_upload_movieid_1 exp02_register_movieid_1 \
+#exp02_web_server_0 exp02_register_test1 exp02_invoker_test1 -- -j 16"
